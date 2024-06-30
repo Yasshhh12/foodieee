7139b28b6860774a48ba02ae12347ac489acd61c
Yasshhh12 <prasyast152@gmail.com>
2024-06-30 12:23:32 +0530

    prepared a form using react

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -4,17 +4,23 @@ import datas from './Data.json'
 const Learn = () => {
 
   const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
 
   const handleName = (e) =>{
     setName(e.target.value); 
+  } 
+
+  const handleEmail = (e) =>{
+    setEmail(e.target.value); 
   } 
   
   return (
     <div>
 
     <label>
-        Name:<input type="text" value={name} className='border border-black' onChange={handleName}/>
-        Email:
+        Name:<input type="text" value={name} className='border border-black' onChange={handleName} />
+        <br/>
+        Email:<input type="email" value={email} className='border border-black' onChange={handleEmail}/>
     </label>
 
 
