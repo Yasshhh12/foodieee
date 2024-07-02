bfede6b69de3a1d22002c8aedabd7bb6b1ef3a7d
Yasshhh12 <prasyast152@gmail.com>
2024-07-02 11:49:47 +0530

    added necessory files

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -2,30 +2,23 @@ import React, { useEffect, useState } from 'react'
 import data from './Data'
 
 const Learn = () => {
-  const [selected, setSelected] = useState(null)
 
-  const handleClickEvent = (setCurrentId) => {
-    setSelected(setCurrentId === selected ? null : setCurrentId)
-  }
+  const [typeOfColor,setTypeOfColor]
+  const [color,setColor] = useState("#000000");
+
+
 
   return (
     <div>
-      <h1>Accordian</h1>
-      <div>
-        {data.map((dataItem, index) => (
-          <div>
-            <div key={index}>
-              <h2 onClick={() => handleClickEvent(dataItem.id)}>
-                {dataItem.question}
-              </h2>
-              <span>+</span>
-            </div>
-            <div>
-              {selected == dataItem.id ? <div>{dataItem.answer}</div> : null}
-            </div>
-          </div>
-        ))}
-      </div>
+      <button onClick={()=>setTyp}>
+        Create Hex color
+      </button>
+      <button>
+        Create Rgb color
+      </button>
+      <button>
+        Generate Random color
+      </button>
     </div>
   )
 }
